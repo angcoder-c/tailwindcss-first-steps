23ef6c4653038fc8b351f7582f17ab0ea2641c36
angc <angcoder.c@gmail.com>
2025-01-13 22:38:22 -0600

    init tailwindcss + react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/navbar"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-slate-700 text-white font-bold text-3xl text-center">App</h1>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+    return (
+        <p>navbar</p>
+    )
+}
+
+export default NavBar
